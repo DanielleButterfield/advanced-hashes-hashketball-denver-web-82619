8bdd21379b46c426904a8b745b3430c1ab0fc907
Danielle Butterfield <butterfieldduncan@gmail.com>
2019-08-23 02:57:40 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,11 +1,15 @@
 # Write your code here!
 
+def game_hash
+  gameHash = {
+    :home => {
+      :team_name => ""
+      :colors => []
+      :players => [
 
-
-
-
-
-
-
-
-
+      ]
+    }
+    :away => {}
+  }
+  return gameHash
+end
